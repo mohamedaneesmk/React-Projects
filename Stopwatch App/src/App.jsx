27ae18f1ebd8a225b28bc234ee0a7d64766ad986
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-29 10:46:01 +0530

    Stop Watch App

---
+++ Stopwatch App/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Stopwatch from './StopWatch.jsx';
+import './index.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
